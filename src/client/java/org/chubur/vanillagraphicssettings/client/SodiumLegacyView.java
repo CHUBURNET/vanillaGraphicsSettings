8de4e0bb87269d61cac798cf775eb4a0b3f37a8d
Artem Chubur <artemchubur@gmail.com>
2026-08-21 19:11:30 +0600

    release version

--- src/client/java/org/chubur/vanillagraphicssettings/client/SodiumLegacyView.java
+++ src/client/java/org/chubur/vanillagraphicssettings/client/SodiumLegacyView.java
@@ -1,13 +1,17 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 package org.chubur.vanillagraphicssettings.client;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-/** Runtime bridge with no Minecraft classes in its signature or constant pool. */
+@Environment(EnvType.CLIENT)
 public final class SodiumLegacyView {
-    private SodiumLegacyView() { }
+    private SodiumLegacyView() {
+    }
 
     public static boolean open(Object sodiumScreen) {
         try {
@@ -17,62 +21,80 @@ public final class SodiumLegacyView {
             Object vanillaScreen = createVanillaScreen(parent, client, options);
             setScreen(client, vanillaScreen);
             return true;
-        } catch (ReflectiveOperationException ignored) {
-            // A future Sodium/Minecraft version changed internals. Keep Sodium's own menu.
+        } catch (ReflectiveOperationException var5) {
             return false;
         }
     }
 
     private static Object minecraftClient() throws ReflectiveOperationException {
-        for (String name : new String[] {"net.minecraft.class_310", "net.minecraft.client.MinecraftClient", "net.minecraft.client.Minecraft"}) {
+        for(String name : new String[]{"net.minecraft.class_310", "net.minecraft.client.MinecraftClient", "net.minecraft.client.Minecraft"}) {
             try {
                 Class<?> type = Class.forName(name);
-                for (Method method : type.getMethods()) {
-                    if (Modifier.isStatic(method.getModifiers()) && method.getParameterCount() == 0 && method.getReturnType() == type
-                            && (method.getName().equals("method_1551") || method.getName().equals("getInstance"))) return method.invoke(null);
+
+                for(Method method : type.getMethods()) {
+                    if (Modifier.isStatic(method.getModifiers()) && method.getParameterCount() == 0 && method.getReturnType() == type && (method.getName().equals("method_1551") || method.getName().equals("getInstance"))) {
+                        return method.invoke((Object)null);
+                    }
                 }
-            } catch (ClassNotFoundException ignored) { }
+            } catch (ClassNotFoundException var9) {
+            }
         }
+
         throw new ClassNotFoundException("Minecraft client");
     }
 
     private static Object findOptions(Object client) throws ReflectiveOperationException {
-        for (Field field : client.getClass().getFields()) {
+        for(Field field : client.getClass().getFields()) {
             String name = field.getName();
-            if (name.equals("field_1690") || name.equals("options")) return field.get(client);
+            if (name.equals("field_1690") || name.equals("options")) {
+                return field.get(client);
+            }
         }
+
         throw new NoSuchFieldException("GameOptions");
     }
 
     private static Object createVanillaScreen(Object parent, Object client, Object options) throws ReflectiveOperationException {
-        for (String name : new String[] {"net.minecraft.class_446", "net.minecraft.client.gui.screen.option.VideoOptionsScreen", "net.minecraft.client.gui.screens.options.VideoSettingsScreen"}) {
+        for(String name : new String[]{"net.minecraft.class_446", "net.minecraft.client.gui.screen.option.VideoOptionsScreen", "net.minecraft.client.gui.screens.options.VideoSettingsScreen"}) {
             try {
                 Class<?> type = Class.forName(name);
-                for (Constructor<?> constructor : type.getConstructors()) {
+
+                for(Constructor<?> constructor : type.getConstructors()) {
                     Object[] arguments = new Object[constructor.getParameterCount()];
                     Class<?>[] parameters = constructor.getParameterTypes();
                     boolean valid = parameters.length >= 2;
-                    for (int index = 0; index < parameters.length && valid; index++) {
-                        if (parent != null && parameters[index].isInstance(parent)) arguments[index] = parent;
-                        else if (parameters[index].isInstance(client)) arguments[index] = client;
-                        else if (parameters[index].isInstance(options)) arguments[index] = options;
-                        else valid = false;
+
+                    for(int index = 0; index < parameters.length && valid; ++index) {
+                        if (parent != null && parameters[index].isInstance(parent)) {
+                            arguments[index] = parent;
+                        } else if (parameters[index].isInstance(client)) {
+                            arguments[index] = client;
+                        } else if (parameters[index].isInstance(options)) {
+                            arguments[index] = options;
+                        } else {
+                            valid = false;
+                        }
                     }
-                    if (valid) return constructor.newInstance(arguments);
+
+                    if (valid) {
+                        return constructor.newInstance(arguments);
+                    }
                 }
-            } catch (ClassNotFoundException ignored) { }
+            } catch (ClassNotFoundException var16) {
+            }
         }
+
         throw new ClassNotFoundException("Vanilla video settings screen");
     }
 
     private static void setScreen(Object client, Object screen) throws ReflectiveOperationException {
-        for (Method method : client.getClass().getMethods()) {
-            if ((method.getName().equals("method_1507") || method.getName().equals("setScreen")) && method.getParameterCount() == 1
-                    && method.getParameterTypes()[0].isInstance(screen)) {
+        for(Method method : client.getClass().getMethods()) {
+            if ((method.getName().equals("method_1507") || method.getName().equals("setScreen")) && method.getParameterCount() == 1 && method.getParameterTypes()[0].isInstance(screen)) {
                 method.invoke(client, screen);
                 return;
             }
         }
+
         throw new NoSuchMethodException("setScreen");
     }
 
--- src/client/java/org/chubur/vanillagraphicssettings/mixin/client/SodiumVideoSettingsMixin.java
+++ src/client/java/org/chubur/vanillagraphicssettings/mixin/client/SodiumVideoSettingsMixin.java
@@ -1,5 +1,8 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 package org.chubur.vanillagraphicssettings.mixin.client;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import org.chubur.vanillagraphicssettings.client.SodiumLegacyView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -7,16 +10,27 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/** Supports Sodium 0.5 (1.20.1) through Sodium 0.8 (1.21.11+). */
+@Environment(EnvType.CLIENT)
 @Pseudo
-@Mixin(targets = {
-        "me.jellysquid.mods.sodium.client.gui.SodiumOptionsGUI",
-        "net.caffeinemc.mods.sodium.client.gui.SodiumOptionsGUI",
-        "net.caffeinemc.mods.sodium.client.gui.VideoSettingsScreen"
-}, remap = false)
+@Mixin(
+        targets = {"me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI", "net/caffeinemc/mods/sodium/client/gui/SodiumOptionsGUI", "net/caffeinemc/mods/sodium/client/gui/VideoSettingsScreen"},
+        remap = false
+)
 abstract class SodiumVideoSettingsMixin {
-    @Inject(method = {"method_25426", "init"}, at = @At("HEAD"), cancellable = true, require = 0, remap = false)
+    SodiumVideoSettingsMixin() {
+    }
+
+    @Inject(
+            method = {"method_25426", "init"},
+            at = {@At("HEAD")},
+            cancellable = true,
+            require = 0,
+            remap = false
+    )
     private void vgs$openVanillaSettings(CallbackInfo ci) {
-        if (SodiumLegacyView.open(this)) ci.cancel();
+        if (SodiumLegacyView.open(this)) {
+            ci.cancel();
+        }
+
     }
 }
